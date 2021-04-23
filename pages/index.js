fa2c2f5fe64d6ce781043d28a4c53313271c63df
Chinedu Ekene Okpala <allstackdev@gmail.com>
2021-04-23 01:58:38 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <Layout
       name='Home'
-      image_url='https://media-exp1.licdn.com/dms/image/C4E0BAQEx9HOPUpq61g/company-logo_200_200/0/1618073512695?e=1626307200&v=beta&t=Gyfh8f5a8pLC2LDj3oHgV9CAk2sytU4V_HEcrzjDOSU'
+      image_url=''
       description='So much of what makes Winchester exceptional is the people who are a part of the school. Also, its beauty - the buildings, the grounds, the setting. Our heritage and archives provide insight into the past, while our community partnerships prepare boys for a future of compassionate leadership.'
     >
       <Hero />
