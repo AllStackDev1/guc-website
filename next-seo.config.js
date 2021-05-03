54a0ab4912ac418a0120d1106eb910250f13ce09
Chinedu Ekene Okpala <allstackdev@gmail.com>
2021-05-03 14:25:39 +0000

    update seo

--- next-seo.config.js
+++ next-seo.config.js
@@ -7,13 +7,13 @@ const config = {
     locale: 'en_IE',
     description:
       'So much of what makes Winchester exceptional is the people who are a part of the school. Also, its beauty - the buildings, the grounds, the setting. Our heritage and archives provide insight into the past, while our community partnerships prepare boys for a future of compassionate leadership.',
-    url: 'https://www.xxx.com',
+    url: 'https://thegcu.com',
     site_name: 'The Goverment College Umuahia',
     images: [
       {
-        url: 'https://i.imgur.com/YlCJLAZ.jpg',
-        width: 1200,
-        height: 622,
+        url: 'https://thegcu.com/images/email/logo.png',
+        width: 376,
+        height: 364,
         alt: 'The Goverment College Umuahia'
       }
     ]
