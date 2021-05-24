8a2f323c1324290de276dbb2da74bc9efc8c84f4
Chinedu Ekene Okpala <allstackdev@gmail.com>
2021-05-24 11:28:30 +0000

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,21 @@
+import { useEffect } from 'react'
 import PropTypes from 'prop-types'
+import TagManager from 'react-gtm-module'
 import { Box, ChakraProvider } from '@chakra-ui/react'
 import { AppContextProvider } from 'context/app'
-
 import { theme } from 'theme/theme'
 
 import '../styles/globals.css'
 
+const tagManagerArgs = {
+  gtmId: process.env.NEXT_PUBLIC_GTM_TRACKING_ID
+}
+
 const MyApp = ({ Component, pageProps }) => {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs)
+  }, [])
+
   return (
     <ChakraProvider theme={theme} resetCSS>
       <AppContextProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-danger */
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import React from 'react'
 
@@ -18,8 +19,23 @@ class MyDocument extends Document {
             crossOrigin=''
           />
           <link href='/fonts/fonts.css' rel='stylesheet' />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NGVKN2K');`
+            }}
+          />
         </Head>
         <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NGVKN2K"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+            }}
+          />
           <Main />
           <NextScript />
         </body>
