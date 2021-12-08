3773c4cf5885a0ce665ff3a9c9939ee76dd82e15
Chinedu Ekene Okpala <allstackdev@gmail.com>
2021-12-08 23:37:14 +0000

    update google tag

--- pages/_document.js
+++ pages/_document.js
@@ -36,15 +36,15 @@ class MyDocument extends Document {
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-NGVKN2K');`
+              'https://www.googletagmanager.com/gtag/js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','G-NMPJQMCKBE');`
             }}
           />
         </Head>
         <body>
           <noscript
             dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NGVKN2K"
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=G-NMPJQMCKBE"
               height="0" width="0" style="display:none;visibility:hidden"></iframe>`
             }}
           />
